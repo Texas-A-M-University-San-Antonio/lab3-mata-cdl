107884ada202eb5ec2a1572f1f8d03fc6f1ae1b2
Jose Mata <jmata021@jaguar.tamu.edu>
2025-09-16 18:36:01 +0000

    Finished InClass and Lab 3

--- Lab3_Jose_Mata.java
+++ Lab3_Jose_Mata.java
@@ -1,13 +1,19 @@
- import java.util.Scanner;   // I2, L2, L3
+ /*
+Student Name: Jose Mata
+Assignment: Lab 3
+Date: Sept. 16, 2025
+Course: CSCI-1136-60L
+Instructor: Al Dungo
+*/
+ 
+ import java.util.Scanner;
  import java.text.NumberFormat;
  
  public class Lab3_Jose_Mata{
 
 
    public static void main(String[] args){
-   
- 
-  Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
         // Declare variables
         double initialValue;   // Initial value of the home
@@ -17,13 +23,13 @@
 
         // Prompt the user for inputs
         System.out.print("Enter the initial value of the home: ");
-        initialValue = input.nextDouble();
+         initialValue = input.nextDouble();
 
         System.out.print("Enter the number of elapsed years: ");
-        years = input.nextInt();
+         years = input.nextInt();
 
         System.out.print("Enter the interest rate (e.g., enter 6 for 6%): ");
-        interestRate = input.nextDouble();
+         interestRate = input.nextDouble();
 
         // Convert interest rate from percentage to decimal
         interestRate = interestRate / 100.0;
